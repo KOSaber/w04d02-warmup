beddb8173a05ed7c4db822cff612b5026df6bba2
KOSaber <k.o.saber@hotmail.com>
2019-12-09 10:08:25 +0300

    2 ways

--- solution.rb
+++ solution.rb
@@ -1,6 +1,10 @@
 def find_difference(s , t)
    
     puts  t.chars- s.chars
+    s= s.split("")
+    t= t.split("")
+    z = t-s
+    puts  z.to_s + " is the letter that was added."
 
     end
     
